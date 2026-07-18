dc9d1d34d817061215326cff907b3c3bbaa6d097
Tarosh <breathofthesun108@gmail.com>
2026-07-18 16:33:40 +0530

    Implement number guessing game in Question4.java

--- Assignments/Assignment1/Question4.java
+++ Assignments/Assignment1/Question4.java
@@ -1,1 +1,26 @@
+import java.util.Scanner;
+public class Question4{
+    public static void main(String[] args) {
+        int correct=27;
 
+        boolean guessed=false;
+        Scanner sc=new Scanner(System.in);
+        for(int i=1;i<=5;i++) {
+            int guess=sc.nextInt();
+
+            if(guess==correct) {
+                System.out.println("Congratulations! You guessed it.");
+                guessed=true;
+                break;
+            }else if(guess<correct) {
+                System.out.println("Too low");
+            }else{
+                System.out.println("Too high");
+            }
+        }
+        if(!guessed) {
+            System.out.println("Better Luck Next Time");
+        }
+
+    }
+}
